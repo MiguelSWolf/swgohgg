baef3010cb755ae18c1ab768f5dd3e51fe5deaef
MiguelSWolf <soulmiguel@gmail.com>
2020-09-14 19:49:59 -0300

    the initial components to make the list

---
+++ src/components/unit.vue
@@ -0,0 +1,98 @@
+<template>
+  <div v-if="unit" class="unit has-gear" :class="`is-gear-${unit.gear}`">
+    <figure class="image is-64x64 is-mask">
+      <img class="is-rounded" :src="unit.photo" />
+    </figure>
+    <div class="expand">
+      <h2>{{ unit.name }}</h2>
+      <h2>{{ formatNumber(unit.gp) }}</h2>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["unit"],
+  methods: {
+    formatNumber(number) {
+      return new Intl.NumberFormat().format(number);
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+.has-gear {
+  .image:after {
+    position: absolute;
+    top: -0px;
+    left: -0px;
+    right: -0px;
+    bottom: -0px;
+    /* background: red; */
+    content: "";
+    display: block;
+  }
+  &.is-gear-1 .image:after {
+    background-image: url(../assets/units/gear-icon-g1.svg);
+  }
+  &.is-gear-2 .image:after {
+    background-image: url(../assets/units/gear-icon-g2.svg);
+  }
+  &.is-gear-3 .image:after {
+    background-image: url(../assets/units/gear-icon-g3.svg);
+  }
+  &.is-gear-4 .image:after {
+    background-image: url(../assets/units/gear-icon-g4.svg);
+  }
+  &.is-gear-5 .image:after {
+    background-image: url(../assets/units/gear-icon-g5.svg);
+  }
+  &.is-gear-6 .image:after {
+    background-image: url(../assets/units/gear-icon-g6.svg);
+  }
+  &.is-gear-7 .image:after {
+    background-image: url(../assets/units/gear-icon-g7.svg);
+  }
+  &.is-gear-8 .image:after {
+    background-image: url(../assets/units/gear-icon-g8.svg);
+  }
+  &.is-gear-9 .image:after {
+    background-image: url(../assets/units/gear-icon-g9.svg);
+  }
+  &.is-gear-10 .image:after {
+    background-image: url(../assets/units/gear-icon-g10.svg);
+  }
+  &.is-gear-11 .image:after {
+    background-image: url(../assets/units/gear-icon-g11.svg);
+  }
+  &.is-gear-12 .image:after {
+    background-image: url(../assets/units/gear-icon-g12.svg);
+  }
+  &.is-gear-13 .image:after {
+    background-image: url(../assets/units/gear-icon-g13.png);
+    background-size: 84px auto;
+    background-position-y: 5px;
+    background-position-x: 3px;
+    top: -12px;
+    left: -12px;
+    right: -12px;
+    bottom: -6px;
+  }
+}
+.unit {
+  position: relative;
+  .expand {
+    display: none;
+    position: absolute;
+    bottom: 100%;
+    background: #fff;
+    border: 1px solid #ccc;
+    border-radius: 2px;
+    z-index: 2;
+  }
+  &:hover .expand {
+    display: inline;
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,147 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="list">
+      <!-- <div v-for="unit in player.ground" :key="unit.id">
+          <unit :unit="unit" />
+      </div> -->
+    </div>
+    <div class="listTeams">
+      <team
+        :team="team"
+        :teams="teams"
+        :showCounter="true"
+        :ground="player.ground"
+        v-for="team in teams"
+        :key="team.name"
+      />
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+/* eslint-disable no-undef */
+// import unit from "../components/unit";
+import team from "../components/team";
 
 export default {
   name: "Home",
+  data: function() {
+    return {
+      player: {
+        data: {
+          name: "",
+          guild: ""
+        },
+        ground: [],
+        ship: [],
+        arenaShip: {
+          rank: 0,
+          lead: "",
+          squad: [],
+          reforcement: []
+        },
+        arenaGround: {
+          rank: 0,
+          lead: "",
+          squad: []
+        }
+      },
+      teams: [
+        {
+          name: "ZBossk",
+          lead: "BOSSK",
+          squadNeeded: [],
+          squadOption: [
+            "AURRA_SING",
+            "BOBAFETT",
+            "JANGOFETT",
+            "CADBANE",
+            "DENGAR",
+            "EMBO",
+            "GREEDO",
+            "GREEFKARGA",
+            "IG88"
+          ],
+          counter: ["ZNute", "ZRey"]
+        },
+        {
+          name: "ZNute",
+          lead: "NUTEGUNRAY",
+          squadNeeded: [],
+          squadOption: ["WATTAMBOR", "COUNTDOOKU", "JANGOFETT", "ENFYSNEST"],
+          counter: []
+        },
+        {
+          name: "ZRey",
+          lead: "REYJEDITRAINING",
+          squadNeeded: [],
+          squadOption: ["BB8", "REY", "FINN", "RESISTANCETROOPER"],
+          counter: []
+        }
+      ],
+      code: 295177617,
+      token: "8e1f5f4742763e2f32ee2a3151f742184382a148",
+      apiSwgoh: "https://api.swgoh.help/swgoh/",
+      loading: false
+    };
+  },
   components: {
-    HelloWorld
+    // unit,
+    team
+  },
+  methods: {
+    getPlayerData: function() {
+      this.loading = true;
+      const headers = {
+        authorization: `Bearer ${this.token}`,
+        "content-type": "application/json"
+      };
+      const data = { allycodes: [this.code] };
+      this.$http
+        // .post(`${this.apiSwgoh}players`, data, { headers })
+        .get("data.json", data, { headers })
+        .then(resource => {
+          this.player.data.name = resource.body[0].name;
+          this.player.data.guild = resource.body[0].guildName;
+          resource.body[0].roster.forEach(element => {
+            if (element.combatType == 1) {
+              let unit = {
+                id: element.defId,
+                name: element.defId,
+                photo: `https://swgoh.gg/game-asset/u/${element.defId}/`,
+                gear: element.gear,
+                rarity: element.rarity,
+                relic: element.relic.currentTier,
+                // skills: element.skills,
+                // mods: element.mods,
+                gp: element.gp,
+                level: element.level
+              };
+              this.player.ground.push(unit);
+            }
+          });
+
+          this.loading = false;
+        });
+    }
+  },
+  mounted() {
+    this.getPlayerData();
   }
 };
 </script>
+<style lang="scss">
+.list {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+}
+.listTeams {
+  .options {
+    display: flex;
+    .unit {
+      transform: scale(0.85);
+    }
+  }
+}
+</style>
