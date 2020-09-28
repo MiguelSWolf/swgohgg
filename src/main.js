04875fcf2e6663a5eac4bbb117e1191eda061087
MiguelSWolf <soulmiguel@gmail.com>
2020-09-28 15:26:26 -0300

    just a test with cors

--- src/main.js
+++ src/main.js
@@ -6,7 +6,16 @@ import store from "./store";
 
 Vue.use(VueResource);
 
+Vue.http.headers.common["Access-Control-Allow-Origin"] = "*";
+Vue.http.headers.common["Access-Control-Allow-Methods"] =
+  "POST, GET, PUT, OPTIONS, DELETE";
+Vue.http.headers.common["Access-Control-Allow-Headers"] =
+  "Access-Control-Allow-Methods, Access-Control-Allow-Origin, Origin, Accept, Content-Type";
+Vue.http.headers.common["Content-Type"] = "application/json";
+Vue.http.headers.common["Accept"] = "application/json";
+
 Vue.config.productionTip = false;
+Window.isDev = process.env.NODE_ENV !== "production";
 
 new Vue({
   router,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,15 +4,41 @@
       <div class="hero-body">
         <div class="container">
           <h1 class="title">
-            <div class="columns is-mobile">
-              <div class="column">
-                {{ attack.name }}
+            <div class="columns is-tablet">
+              <div class="column is-3">
+                <div class="field">
+                  <input
+                    class="input is-fullwidth is-large"
+                    v-model="attackCode"
+                  />
+                </div>
+                <div class="field">
+                  {{ attack.name }}
+                </div>
               </div>
-              <div class="column">
+              <div
+                class="column is-1"
+                style="text-align:center; line-height: 45px"
+              >
                 VS.
               </div>
-              <div class="column">
-                {{ defence.name }}
+              <div class="column is-3">
+                <div class="field">
+                  <input
+                    class="input is-fullwidth is-large"
+                    v-model="defenceCode"
+                  />
+                </div>
+                <div class="field">
+                  {{ defence.name }}
+                </div>
+              </div>
+              <div class="column is-3">
+                <input
+                  class="button is-info is-large is-fullwidth"
+                  value="Search"
+                  @click="getPlayers"
+                />
               </div>
             </div>
           </h1>
@@ -22,14 +48,14 @@
     <section class="section">
       <div class="container">
         <h1 class="title">Teams</h1>
-        <card-team :team="team" v-for="team in teams" :key="team.id" />
+        <!-- <card-team :team="team" v-for="team in teams" :key="team.id" /> -->
       </div>
     </section>
   </div>
 </template>
 
 <script>
-import cardTeam from "../components/team";
+// import cardTeam from "../components/team";
 import abilities from "../assets/baseData/abilities.json";
 import counters from "../assets/baseData/counters.json";
 import units from "../assets/baseData/characters.json";
@@ -41,7 +67,14 @@ import player884435532 from "../assets/884435532.json";
 export default {
   name: "Home",
   components: {
-    cardTeam
+    // cardTeam
+  },
+  data() {
+    return {
+      attackCode: "",
+      defenceCode: "",
+      loading: false
+    };
   },
   methods: {
     getBaseInfos: function() {
@@ -49,14 +82,37 @@ export default {
       this.$store.dispatch("setAbilities", abilities);
       this.$store.dispatch("setTeams", counters);
     },
-    getPlayerData: function() {
-      // TODO
-      // https://swgoh.gg/api/player/{ally_code}/
-      // 166159449
-    },
     getPlayersTest: function() {
       this.$store.dispatch("setPlayer", player166159449);
+      this.attackCode = "166159449";
       this.$store.dispatch("setPlayer", player884435532);
+      this.defenceCode = "884435532";
+    },
+    getPlayers: function() {
+      this.loading = true;
+      console.log("start first");
+      this.$http
+        .get(`https://swgoh.gg/api/player/166159449`)
+        .then(responseAttack => {
+          console.log({ responseAttack: responseAttack });
+          this.$store.dispatch("setPlayer", responseAttack);
+          console.log("start second");
+          this.$http
+            .get(`https://swgoh.gg/api/player/${this.defenceCode}/`)
+            .then(responseDefence => {
+              console.log({ responseDefence: responseDefence });
+              this.$store.dispatch("setPlayer", responseDefence);
+              this.loading = false;
+            })
+            .catch(err => {
+              console.error(err);
+              this.loading = false;
+            });
+        })
+        .catch(err => {
+          console.error(err);
+          this.loading = false;
+        });
     }
   },
   computed: {
@@ -75,8 +131,19 @@ export default {
     })
   },
   mounted() {
+    this.attackCode = "166159449";
+    this.defenceCode = "884435532";
+    if (Window.isDev) {
+      console.log("Versão dev");
+    } else {
+      console.log("Versão prod");
+    }
     this.getBaseInfos();
-    this.getPlayersTest();
+    // if (Window.isDev) {
+    //   this.getPlayersTest();
+    // } else {
+    // this.getPlayers();
+    // }
   }
 };
 </script>
