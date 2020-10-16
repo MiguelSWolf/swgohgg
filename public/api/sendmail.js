0822fe4fc832d5e819f4990f7fd00c0d96b6225b
MiguelSWolf <soulmiguel@gmail.com>
2020-10-16 12:19:49 -0300

    another test

--- public/api/sendmail.js
+++ public/api/sendmail.js
@@ -2,7 +2,7 @@ const sgMail = require("@sendgrid/mail");
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 const msg = {
   to: "soulmiguel@gmail.com",
-  from: "test@example.com", // Use the email address or domain you verified above
+  from: "soulmiguel@gmail.com", // Use the email address or domain you verified above
   subject: "Sending with Twilio SendGrid is Fun",
   text: "and easy to do anywhere, even with Node.js",
   html: "<strong>and easy to do anywhere, even with Node.js</strong>"
