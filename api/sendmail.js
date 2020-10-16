9b2cb0822d4c138e5a1bd2b9e67d2eb8069dd7c3
MiguelSWolf <soulmiguel@gmail.com>
2020-10-16 14:21:27 -0300

    try another form of serverless function

--- api/sendmail.js
+++ api/sendmail.js
@@ -7,16 +7,38 @@ const msg = {
   text: "and easy to do anywhere, even with Node.js",
   html: "<strong>and easy to do anywhere, even with Node.js</strong>"
 };
-//ES6
-function send() {
-  sgMail.send(msg).then(
-    () => {},
-    error => {
-      console.error(error);
-      if (error.response) {
-        console.error(error.response.body);
-      }
-    }
+
+const allowCors = fn => async (request, response) => {
+  response.setHeader("Access-Control-Allow-Credentials", true);
+  response.setHeader("Access-Control-Allow-Origin", "*");
+  // another option
+  // response.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  response.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS");
+  response.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
   );
-}
-module.exports = send();
+  if (request.method === "OPTIONS") {
+    response.status(200).end();
+    return;
+  }
+  return await fn(request, response);
+};
+
+const handler = async function(request, response) {
+  sgMail
+    .send(msg)
+    .then(res => {
+      response.statusCode = 200;
+      response.setHeader("Content-Type", `application/json`);
+      response.end(JSON.stringify({ message: "sended!", return: res }));
+    })
+    .catch(error => {
+      console.error();
+      response.statusCode = 500;
+      response.setHeader("Content-Type", `application/json`);
+      response.end(JSON.stringify({ message: "error!", error: error }));
+    });
+};
+
+module.exports = allowCors(handler);
