b43d42e2d2405c607c13aef0c96e4030d1ec7527
MiguelSWolf <soulmiguel@gmail.com>
2020-11-15 17:51:42 -0300

    improves mods maps

--- src/components/tableModMaps.vue
+++ src/components/tableModMaps.vue
@@ -6,25 +6,49 @@
       >
         <tr>
           <th>Toon</th>
-          <th>Square</th>
+          <th></th>
           <th>Arrow</th>
-          <th>Rhombus</th>
           <th>Triangle</th>
           <th>Circle</th>
           <th>Cross</th>
+          <th>Square</th>
+          <th>Rhombus</th>
         </tr>
-        <tr v-for="unit in unitsSorted" :key="unit.defId">
-          <td>
-            {{ unit.defId }}<br />
-            {{ formatNumber(unit.gp) }}
-          </td>
-          <td v-for="mod in unit.mods" :key="mod.id">
-            {{ mod.primaryStat.value }}
-            {{ statsName(mod.primaryStat.unitStat) }}
-            <br />
-            Speed: {{ showSpeed(mod.secondaryStat) }}
-          </td>
-        </tr>
+        <template v-for="unit in unitsSorted">
+          <tr :key="`${unit.defId}1`">
+            <th rowspan="3">
+              {{ unit.defId }}
+            </th>
+            <td>{{ unit.bonus[0] }}</td>
+            <td>{{ statsName(unit.mods, 2) }}</td>
+            <td>{{ statsName(unit.mods, 4) }}</td>
+            <td>{{ statsName(unit.mods, 5) }}</td>
+            <td>{{ statsName(unit.mods, 6) }}</td>
+            <td>{{ statsName(unit.mods, 1) }}</td>
+            <td>{{ statsName(unit.mods, 3) }}</td>
+          </tr>
+          <tr :key="`${unit.defId}2`">
+            <td>{{ unit.bonus[1] }}</td>
+            <td>{{ statsValue(unit.mods, 2) }}</td>
+            <td>{{ statsValue(unit.mods, 4) }}</td>
+            <td>{{ statsValue(unit.mods, 5) }}</td>
+            <td>{{ statsValue(unit.mods, 6) }}</td>
+            <td>{{ statsValue(unit.mods, 1) }}</td>
+            <td>{{ statsValue(unit.mods, 3) }}</td>
+          </tr>
+          <tr :key="`${unit.defId}3`">
+            <td>{{ unit.bonus[2] || "" }}</td>
+            <td>{{ showSpeed(unit.mods, 2) }}</td>
+            <td>{{ showSpeed(unit.mods, 4) }}</td>
+            <td>{{ showSpeed(unit.mods, 5) }}</td>
+            <td>{{ showSpeed(unit.mods, 6) }}</td>
+            <td>{{ showSpeed(unit.mods, 1) }}</td>
+            <td>{{ showSpeed(unit.mods, 3) }}</td>
+          </tr>
+          <tr :key="`${unit.defId}4`">
+            <td colspan="8" style="min-height: 20px"></td>
+          </tr>
+        </template>
       </table>
     </div>
   </div>
@@ -37,7 +61,19 @@ export default {
     return {
       units: [],
       name: "",
-      guild: ""
+      guild: "",
+      teams: [
+        {
+          name: "IT",
+          units: [
+            "VEERS",
+            "COLONELSTARCK",
+            "ADMIRALPIETT",
+            "RANGETROOPER",
+            "DEATHTROOPER"
+          ]
+        }
+      ]
     };
   },
   computed: {
@@ -61,6 +97,11 @@ export default {
     formatNumber(number) {
       return new Intl.NumberFormat().format(number);
     },
+    findMod(mods, slot) {
+      return mods.find(mod => {
+        return mod.slot == slot;
+      });
+    },
     slotName(slot) {
       switch (slot) {
         case 1:
@@ -79,18 +120,25 @@ export default {
           return "Not defined";
       }
     },
-    statsName(statsNumber) {
+    statsName(mods, slot) {
+      const mod = this.findMod(mods, slot);
+      if (mod != null) {
+        return this.translateAttribute(mod.primaryStat.unitStat);
+      }
+      return "-";
+    },
+    translateAttribute(statsNumber) {
       switch (statsNumber) {
         case 48:
-          return "% offence";
+          return "offense";
         case 5:
           return "speed";
         case 49:
-          return "% defence";
+          return "defense";
         case 55:
-          return "% health";
+          return "health";
         case 18:
-          return "% tenacity";
+          return "tenacity";
         case 16:
           return "Critical Damage";
         case 56:
@@ -103,56 +151,100 @@ export default {
           return "Accuracy";
         case 54:
           return "Critical Avoidance";
-        // case : return "";
-        // case : return "";
-        // case : return "";
-        // case : return "";
-        // case : return "";
-        // case : return "";
-        // case : return "";
-        // case : return "";
         default:
           return `(${statsNumber}) is not defined`;
       }
     },
-    showSpeed(stats) {
-      let value = "-";
-      stats.forEach(stat => {
-        if (stat.unitStat == 5) {
-          console.log(stat);
-          value = stat.value;
-        }
-      });
-      return value;
+    showSpeed(mods, slot) {
+      const mod = this.findMod(mods, slot);
+      if (mod != null) {
+        let value = "0";
+        mod.secondaryStat.forEach(stat => {
+          if (stat.unitStat == 5) {
+            value = stat.value;
+          }
+        });
+        return `Speed: ${value}`;
+      }
+      return "-";
+    },
+    statsValue(mods, slot) {
+      const mod = this.findMod(mods, slot);
+      if (mod != null) {
+        return mod.primaryStat.value;
+      }
+      return "-";
     }
   },
   mounted() {
-    this.units = players[0].roster;
+    players[0].roster.forEach(unit => {
+      let sets = [];
+      unit.mods.forEach(mod => {
+        switch (mod.set) {
+          case 4:
+            sets.push("Speed");
+            break;
+          case 6:
+            sets.push("Crit Damage");
+            break;
+          case 1:
+            sets.push("Health");
+            break;
+          case 2:
+            sets.push("Offense");
+            break;
+          case 3:
+            sets.push("Defense");
+            break;
+          case 5:
+            sets.push("Crit Chance");
+            break;
+          case 7:
+            sets.push("Potency");
+            break;
+          case 8:
+            sets.push("Tenacity");
+            break;
+          default:
+            sets.push(mod.set);
+            break;
+        }
+      });
+      sets.sort();
+      let bonus = [];
+      let countSet = 0;
+      let oldSet = "";
+      console.log("================");
+      console.log(sets);
+      sets.forEach(set => {
+        console.log({ set, oldSet, countSet });
+        if (oldSet == set) {
+          countSet++;
+        } else {
+          oldSet = set;
+          countSet++;
+        }
+        if (countSet >= 2) {
+          if (
+            !(
+              (oldSet == "Speed" ||
+                oldSet == "Offense" ||
+                oldSet == "Crit Damage") &&
+              countSet != 4
+            )
+          ) {
+            bonus.push(`${countSet}x ${oldSet}`);
+            oldSet = "";
+            countSet = 0;
+          }
+        }
+      });
+      unit.sets = sets;
+      unit.bonus = bonus.sort();
+      this.units.push(unit);
+    });
     this.name = players[0].name;
     this.guild = players[0].guild;
   }
 };
 </script>
-<style lang="scss">
-.is-matchmaker {
-  background: #fff;
-  border: 1px solid #ccc;
-  border-radius: 8px;
-  padding: 12px;
-  &.is-deleted {
-    filter: grayscale(100%);
-  }
-  .collection-char {
-    height: 126px;
-  }
-  .collection-char-name {
-    display: none;
-  }
-}
-.is-positive {
-  color: green;
-}
-.is-negative {
-  color: red;
-}
-</style>
