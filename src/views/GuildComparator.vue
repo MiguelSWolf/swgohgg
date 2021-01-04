b345c6e80e041e3fc2c8ff54529f24ba2adf27b5
MiguelSWolf <soulmiguel@gmail.com>
2021-01-04 17:32:18 -0300

    new TW

--- src/views/GuildComparator.vue
+++ src/views/GuildComparator.vue
@@ -55,7 +55,7 @@
 
 <script>
 import yourGuild from "../assets/Despertar.json";
-import opponentGuild from "../assets/The Republıc.json";
+import opponentGuild from "../assets/TEP_Yavin IV.json";
 import ResumeTab from "../components/comparator/resume";
 import TeamsTab from "../components/comparator/teams";
 import ArenaTab from "../components/comparator/arena";
