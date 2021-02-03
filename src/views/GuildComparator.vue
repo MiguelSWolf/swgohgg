ab2f7f68ccf3fa12a77d266669a8c89427d4f5b4
MiguelSWolf <soulmiguel@gmail.com>
2021-02-03 17:16:49 -0300

    new tw

--- src/views/GuildComparator.vue
+++ src/views/GuildComparator.vue
@@ -55,7 +55,7 @@
 
 <script>
 import yourGuild from "../assets/Despertar.json";
-import opponentGuild from "../assets/Dying Light.json";
+import opponentGuild from "../assets/Order of the Gray Jedeye.json";
 import ResumeTab from "../components/comparator/resume";
 import TeamsTab from "../components/comparator/teams";
 import ArenaTab from "../components/comparator/arena";
